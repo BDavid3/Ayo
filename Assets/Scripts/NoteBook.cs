9e6e324fbe0fe6a6a06563620bf4f0d5c45bcb68
David <gubamagos@gmail.com>
2026-06-02 18:01:26 +0200

    Updates

---
+++ Assets/Scripts/NoteBook.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class NoteBook : MonoBehaviour
+{
+    public GameObject canvas;
+    private Outline _outline;
+    public bool isEnabled;
+
+    [SerializeField] private PlayerMovementScript playerMovementScript;
+    [SerializeField] private RotateCamera camScript;
+
+    private void Start()
+    {
+        _outline = GetComponent<Outline>();
+    }
+
+    void Update()
+    {
+        if (_outline.enabled && Input.GetKeyDown(KeyCode.E))
+        {
+            isEnabled = !isEnabled;
+            
+            Cursor.visible = isEnabled;
+            Cursor.lockState = isEnabled ? CursorLockMode.None : CursorLockMode.Locked;
+
+            playerMovementScript.enabled = !isEnabled;
+            camScript.enabled = !isEnabled;
+            
+            canvas.SetActive(isEnabled);
+            
+        }
+    }
+}
---
+++ Assets/Tasks.cs
@@ -0,0 +1,3 @@
+// NoteBook change the Again E command to Button Exit
+// Inheritance in the interactable script 
+// Perhaps merging the movement + camera script, and the others 
