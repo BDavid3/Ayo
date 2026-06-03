03f8b64b38ebdad8f24fcbadff38fbe391f72e69
David <gubamagos@gmail.com>
2026-06-03 18:47:17 +0200

    Merging the camera and the playerMovement Script into one Script.

---
+++ Assets/Scripts/PlayerAndCameraMovement.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class PlayerAndCameraMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Transform cam;
+    private Vector3 _movement;
+    
+    [SerializeField] private float sens;
+    [SerializeField] private float distance = 3f;
+    [SerializeField] private LayerMask collisionMask;
+    private float _yaw;
+    private float _pitch;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float  horizontal = Input.GetAxis("Horizontal"); 
+        float vertical = Input.GetAxis("Vertical"); 
+        
+        Vector3 camForward = cam.forward; 
+        Vector3 camRight = cam.right; 
+        
+        camForward.y = 0f;
+        camRight.y = 0f;
+        
+        _movement = (camForward * vertical + camRight * horizontal).normalized;
+        
+    }
+    
+    void LateUpdate()
+    {
+        _yaw += Input.GetAxis("Mouse X") * sens;
+        _pitch -= Input.GetAxis("Mouse Y") * sens;
+        _pitch = Mathf.Clamp(_pitch, -20f, 60f);
+
+        Quaternion rotation = Quaternion.Euler(_pitch, _yaw, 0f);
+        Vector3 desiredOffset = rotation * new Vector3(0f, 0f, -distance);
+        Vector3 desiredPosition = transform.position + desiredOffset;
+        
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, desiredOffset.normalized, out hit, distance, collisionMask))
+        {
+            cam.position = hit.point + hit.normal * 0.2f;
+        }
+        else
+        {
+            cam.position = desiredPosition;
+        }
+        
+        cam.LookAt(transform);
+    }
+    
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + _movement * (speed * Time.fixedDeltaTime));
+    }
+    
+    // Update is frame dependent, FixedUpdate is not. 
+    
+}
--- Assets/Tasks.cs
+++ Assets/Tasks.cs
@@ -1,3 +1,5 @@
 // NoteBook change the Again E command to Button Exit
 // Inheritance in the interactable script 
 // Perhaps merging the movement + camera script, and the others 
+
+// After Load file -> Set cursor disabled and locekd -> and also the intro
